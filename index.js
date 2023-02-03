de3386f9a0783a1912012d3669c4c61302bd92ea
arsh459 <arshgoyal459@gmail.com>
2023-02-03 21:57:52 +0530

    Home page filter and sort

--- index.js
+++ index.js
@@ -1,15 +1,76 @@
 window.onload= function(){
     let url="http://localhost:3000/Restaurants";
-    fetchData(url);
+    fetchData(url);  
+    document.querySelector(".cuisinefilter").addEventListener("click",function(){
+        window.location.href="filter.html";
+    })
+    document.querySelector(".relevance").addEventListener("click",function(){
+        let modifiedata=JSON.parse(localStorage.getItem("mRestraunts"));
+        sortByRelevance(modifiedata);
+    })
+    document.querySelector(".delivery-time").addEventListener("click",function(){
+        let modifiedata=JSON.parse(localStorage.getItem("mRestraunts"));
+        sortBydelivery(modifiedata);
+    })
+    document.querySelector(".rating").addEventListener("click",function(){
+        let modifiedata=JSON.parse(localStorage.getItem("mRestraunts"));
+        sortByrating(modifiedata);
+    })
+    document.querySelector(".cost-lh").addEventListener("click",function(){
+        let modifiedata=JSON.parse(localStorage.getItem("mRestraunts"));
+        sortBycostlh(modifiedata);
+    })
+    document.querySelector(".cost-hl").addEventListener("click",function(){
+        let modifiedata=JSON.parse(localStorage.getItem("mRestraunts"));
+        sortBycosthl(modifiedata);
+    })
+    carousel.addEventListener("mousedown", dragStart);
+    carousel.addEventListener("touchstart", dragStart);
+    document.addEventListener("mousemove", dragging);
+    carousel.addEventListener("touchmove", dragging);
+    document.addEventListener("mouseup", dragStop);
+    carousel.addEventListener("touchend", dragStop);
 }
 
 async function fetchData(url){
     let response=await fetch(url);
     let restaurantsData=await response.json();
-    console.log(restaurantsData);
-    displayHome(restaurantsData);
+    localStorage.setItem("Restraunts",JSON.stringify(restaurantsData));
+    let modifiedata=JSON.parse(localStorage.getItem("mRestraunts")) || [];
+    if(modifiedata.length==0){
+        localStorage.setItem("mRestraunts",JSON.stringify(restaurantsData));
+        modifiedata=JSON.parse(localStorage.getItem("mRestraunts"));
+    }
+    display();
+    
 }
+function display(){
+    let filtersarr=JSON.parse(localStorage.getItem("filters")) || [];
+    if(filtersarr.length==0){
+        let modifiedata=JSON.parse(localStorage.getItem("Restraunts"));
+        localStorage.setItem("mRestraunts",JSON.stringify(modifiedata));
+        displayHome(modifiedata);
+    }
+    else{
+        var arr=JSON.parse(localStorage.getItem("mRestraunts")) || [];
+        arr=arr.filter(function(e,i){
+            let flag=true;
+            for(let i=0;i<filtersarr.length;i++){
+                if(e.cuisine.includes(filtersarr[i])){
 
+                }
+                else{
+                    flag=false;
+                }
+            }
+            if(flag==true){
+                return e;
+            }
+        })
+        localStorage.setItem("mRestraunts",JSON.stringify(arr));
+        displayHome(arr);
+    }
+}
 function displayHome(data){
     document.querySelector(".restaurant-list").textContent="";
     data.forEach(function(e,i){
@@ -17,7 +78,10 @@ function displayHome(data){
         div.setAttribute("class","place");
 
         var a=document.createElement("a");
-        a.href="3bsHYHMj4xyKyfPHP1NfPMYGF4SG2M5DX3VN9eEPRWdd"
+        a.href="restaurantPage.html"
+        a.addEventListener("click",function(){
+            localStorage.setItem("restaurantId",e.id);
+        })
         a.setAttribute("class","place-link");
 
         div.append(a);
@@ -79,7 +143,13 @@ function displayHome(data){
         span.append(i);
 
         var span2=document.createElement("span");
-        span.textContent=e.ratings;
+        span.textContent="★   "+"  "+e.ratings;
+        if(e.ratings>4){
+            div11.style.backgroundColor="green"
+        }
+        else{
+            div11.style.backgroundColor="orange"
+        }
 
         div11.append(span,span2);
 
@@ -87,7 +157,7 @@ function displayHome(data){
         div12.textContent="•";
 
         var div13=document.createElement("div");
-        div13.textContent=e.approxDeliveryTime;
+        div13.textContent=e.approxDeliveryTime+" min";
 
         var div14=document.createElement("div");
         div14.textContent="•";
@@ -130,6 +200,90 @@ function displayHome(data){
         div3.append(div4,div5,div7,div10,div16);
 
         document.querySelector(".restaurant-list").append(div);
+        document.querySelector(".number").textContent=data.length+" Restaurants";
     });
 }
+
+function sortByRelevance(data){
+    data.sort(function(a,b){
+        if(a.name>b.name) return 1;
+        if(a.name<b.name) return -1;
+        if(a.name==b.name) return 0;
+    });
+    localStorage.setItem("mRestraunts",JSON.stringify(data));
+    displayHome(data);
+}
+
+function sortBycostlh(data){
+    data.sort(function(a,b){
+        if(a.approxPrice>b.approxPrice) return 1;
+        if(a.approxPrice<b.approxPrice) return -1;
+        if(a.approxPrice==b.approxPrice) return 0;
+    });
+    localStorage.setItem("mRestraunts",JSON.stringify(data));
+    displayHome(data);
+}
+function sortBydelivery(data){
+    data.sort(function(a,b){
+        if(a.approxDeliveryTime>b.approxDeliveryTime) return 1;
+        if(a.approxDeliveryTime<b.approxDeliveryTime) return -1;
+        if(a.approxDeliveryTime==b.approxDeliveryTimes) return 0;
+    });
+    localStorage.setItem("mRestraunts",JSON.stringify(data));
+    displayHome(data);
+}
+function sortByrating(data){
+    data.sort(function(a,b){
+        if(a.ratings>b.ratings) return -1;
+        if(a.ratings<b.ratings) return 1;
+        if(a.ratings==b.ratings) return 0;
+    });
+    localStorage.setItem("mRestraunts",JSON.stringify(data));
+    displayHome(data);
+}
+function sortBycosthl(data){
+    data.sort(function(a,b){
+        if(a.approxPrice>b.approxPrice) return -1;
+        if(a.approxPrice<b.approxPrice) return 1;
+        if(a.approxPrice==b.approxPrice) return 0;
+    });
+    localStorage.setItem("mRestraunts",JSON.stringify(data));
+    displayHome(data);
+}
+
+let carousel = document.querySelector(".carousel"),
+firstImg = carousel.querySelectorAll("img")[0],
+arrowIcons = document.querySelectorAll(".wrapper i");
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+function autoSlide(){
+    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
+    positionDiff = Math.abs(positionDiff);
+    let firstImgWidth = firstImg.clientWidth + 14;
+    let valDifference = firstImgWidth - positionDiff;
+    if(carousel.scrollLeft > prevScrollLeft) {
+        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    }
+    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+}
+function dragStart(e){
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+function dragging(e){
+    if(!isDragStart) return;
+    e.preventDefault();
+    isDragging = true;
+    carousel.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+function dragStop(){
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+    if(!isDragging) return;
+    isDragging = false;
+    autoSlide();
+}
+
 
